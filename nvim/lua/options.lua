25ffa21c4408a0233f4cc7837eca6ff70ba9b7bf
Naafiyan Ahmed <naafiyan.ahmed@mongodb.com>
2025-02-20 19:43:49 +0000

    [nvim] cursor block

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -20,6 +20,9 @@ vim.opt.completeopt = {"menu", 'menuone', 'noselect'}
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.statuscolumn = '%=%{v:relnum?v:relnum:v:lnum} '
+
+-- text cursor settings
+vim.opt.guicursor = "n-v-i-c:block-Cursor"
 --
 -- THEME
 -- vim.cmd.colorscheme "catppuccin-mocha"
